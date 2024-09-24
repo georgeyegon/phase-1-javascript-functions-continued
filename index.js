fcfaa272b353b7aab513aa37e361cb5a8da4a607
George Yegon <georgeyegon67@gmail.com>
2024-09-24 15:13:50 +0300

    Added javascript -functions -continued

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Testing the function
+  console.log(saturdayFun()); // "This Saturday, I want to roller-skate!"
+  console.log(saturdayFun("paint")); // "This Saturday, I want to paint!"
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Testing the function
+  console.log(mondayWork()); // "This Monday, I will go to the office."
+  console.log(mondayWork("work from home")); // "This Monday, I will work from home."
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Testing the function
+  console.log(mondayWork()); // "This Monday, I will go to the office."
+  console.log(mondayWork("work from home")); // "This Monday, I will work from home."
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Testing the function
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer")); // "You are !!!a dedicated programmer!!!"
+  console.log(wrapAdjective("%")("an awesome developer")); // "You are %an awesome developer%!"
+  
